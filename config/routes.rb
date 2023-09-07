6adb9d0224d0f75727c05a0aca7095229c6ca89f
abiola-arobieke <abiola.arobieke@gmail.com>
2023-09-08 00:22:44 +0100

    Update routes.rb file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
-Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+# Rails.application.routes.draw do
+#   root 'root#index'
+#   # get 'root/index'
+#   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+#   # Defines the root path route ("/")
+#   # root "articles#index"
+# end
+Rails.application.routes.draw do
+  # root 'root#index'
+  namespace :api do
+    namespace :v1 do
+      get 'messages/random'
+    end
+  end
 end
